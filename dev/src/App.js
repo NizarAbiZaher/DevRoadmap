06e6d3ca5248c65e593caa9558230d645df2297e
NizarAbiZaher <Nizabizaher@gmail.com>
2023-09-06 07:23:37 -0400

    landing

--- dev/src/App.js
+++ dev/src/App.js
@@ -6,9 +6,12 @@ import harvard from './images/harvard.jpg';
 import js from './images/js.png';
 import free from './images/free.jpg';
 import rea from './images/react.png';
-
-
-
+import back from './images/mon.png'
+import gi from './images/git.png';
+import api from './images/api.png';
+import soft from './images/soft.png';
+import data from './images/yes.svg';
+import Footer from './components/Footer';
 
 function App() {
   const [courseData, setCourseData] = useState([])
@@ -18,10 +21,10 @@ function App() {
       {
         number: '01',
         title: 'Introduction to Programming',
-        description: '...',
+        description: 'Start your programming journey with the world renowned CS50 course by Harvard University.',
         content: [
           {
-            link: 'https://www.edx.org/plp/introduction-computer-science-harvardx-cs50x...',
+            link: 'https://www.edx.org/plp/introduction-computer-science-harvardx-cs50x?utm_source=google&utm_campaign=19315581336&utm_medium=cpc&utm_term=cs50&hsa_acc=7245054034&hsa_cam=19315581336&hsa_grp=144242542723&hsa_ad=671177474143&hsa_src=g&hsa_tgt=kwd-296840910&hsa_kw=cs50&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=Cj0KCQjwusunBhCYARIsAFBsUP8cwaHfo0DWEIii5xgqqbSZN-2jPNUtmA_Qemwc7QVrUCs86r-bSM4aAqFHEALw_wcB',
             imageSrc: harvard
           }
         ]
@@ -29,37 +32,60 @@ function App() {
       {
         number: '02',
         title: 'Web Development Basics',
-        description: '...',
+        description: 'Learn the basics of how the web works by using HTML CSS & JavaScript.',
         content: [
           {
             link: 'https://www.freecodecamp.org/learn/2022/responsive-web-design/',
             imageSrc: free
           },
           {
-            link: 'https://www.freecodecamp.org/learn/2022/responsive-web-design/',
-            imageSrc: free
-          }
-        ]
-      },
-      {
-        number: '03',
-        title: 'First Programming Language',
-        description: '...',
-        content: [
-          {
-            link: 'https://www.edx.org/plp/introduction-computer-science-harvardx-cs50x...',
+            link: 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/',
             imageSrc: js
           }
         ]
       },
       {
-        number: '04',
+        number: '03',
         title: 'Frameworks',
-        description: '...',
+        description: 'Explore the fundamentals of building dynamic web applications with React, a powerful JavaScript library for creating user interfaces.',
         content: [
           {
-            link: 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/',
+            link: 'https://fullstackopen.com/en/',
             imageSrc: rea
+          }
+        ]
+      },
+      {
+        number: '04',
+        title: 'Backend & APIs',
+        description: 'Discover the core principles of server-side development by diving into backend technologies and APIs such as User Authentication, Databases, and much more.',
+        content: [
+          {
+            link: 'https://www.freecodecamp.org/learn/back-end-development-and-apis/',
+            imageSrc: back
+          },
+          {
+            link: 'https://youtu.be/WXsD0ZgxjRw?si=0Nlg3DYqjFxEbyHx',
+            imageSrc: api
+          }
+        ]
+      },
+      {
+        number: '05',
+        title: "Finishing Touches",
+        description: "Finish your learning journey with some industry standard skills such as Git & Software Design.",
+        content: [
+          {
+            link: 'https://youtu.be/RGOj5yH7evk?si=TpjCQx09Td3VoPsT',
+            imageSrc: gi,
+          },
+          {
+            link: 'https://www.youtube.com/playlist?list=PL4JxLacgYgqTgS8qQPC17fM-NWMTr5GW6',
+            imageSrc: soft
+          },
+          {
+            link: 'https://www.youtube.com/watch?v=8hly31xKli0',
+            imageSrc: data
           }
         ]
       }
@@ -71,11 +97,13 @@ setCourseData(updatedCourseData)
   return (
     <div className="App">
       <Navbar />
+      <p className='text'>Welcome to the Developer Roadmap! This journey is a completely free way to learn how to code and become a Fullstack developer. This journey should take you anywhere from 5-8 Months. Happy Coding! </p>
       <div className="main">
         {courseData.map((course) => (
           <Main key={course.number} {...course} />
         ))}
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ dev/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='links'>
+            <div className='text'>
+                <a href='https://www.youtube.com/@NizzyABI'><i className="fab fa-youtube"></i></a>
+                <a href='https://twitter.com/NizzyABI'><i className="fa-brands fa-x-twitter"></i></a>
+                <a href='https://discord.gg/te9MBNYq'><i className="fab fa-discord"></i></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
